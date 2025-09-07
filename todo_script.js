217d96011002f0ab4dcfe6756c11af279365c6da
varadisthedev <varadisthedev@gmail.com>
2025-09-07 23:25:35 +0530

    bugfix: completed tasks now directly go in the 'completed tasks' sections, instead of just disappearing

--- todo_script.js
+++ todo_script.js
@@ -2,23 +2,20 @@
 const list=document.getElementById("ongoing_list")
 const taskInput=document.getElementById("taskInput")
 const button=document.getElementById("taskbutton")
-const on_list=document.getElementById("completed_list")
+const c_list=document.getElementById("completed_list")
 
 const adder=()=>{
     const item=document.createElement("li")
     item.textContent = taskInput.value;
     list.appendChild(item)
     //making it so that list item deletes when tapped on them 
-    item.addEventListener("click",function(){
-        item.remove();
-    })
     
     taskInput.value=""
 }
 //event delegation to check the list items
 list.addEventListener("click",(e) => {
   if (e.target.tagName === "LI") {
-    e.target.remove();
+    c_list.appendChild(e.target);
   }
 });
 
