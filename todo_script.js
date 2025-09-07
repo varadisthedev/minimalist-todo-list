901dd344f62a20eaa8e5647c4d1f28a83638e2f6
varadisthedev <varadisthedev@gmail.com>
2025-09-08 00:26:19 +0530

    feature: a 'delete all' button for both finished and ongoing tasks

--- todo_script.js
+++ todo_script.js
@@ -1,41 +1,51 @@
+//TODO: add a checkbox for each o_list items, 
+//TODO: change the font of list items, maybe add a gray color scheme 
+//TODO: add a delete all button
+
+
 //adding itesm to the list (core of TODO)
-const list=document.getElementById("ongoing_list")
+const o_list=document.getElementById("ongoing_list")
+const c_list=document.getElementById("completed_list")
 const taskInput=document.getElementById("taskInput")
 const button=document.getElementById("taskbutton")
-const c_list=document.getElementById("completed_list")
+const c_delete=document.getElementById("c_delete")
+const o_delete=document.getElementById("o_delete")
+
 
 const adder=()=>{
     const item=document.createElement("li")
     item.textContent = taskInput.value;
-    list.appendChild(item)
-    //making it so that list item deletes when tapped on them 
-    
-    taskInput.value=""
+    o_list.appendChild(item) 
+    taskInput.value="" //resetting the input box
 }
-//event delegation to check the list items
-list.addEventListener("click",(e) => {
+
+
+
+const c_deletion=()=>{
+  //for each element in <ul> "c_list" use the .remove()
+  c_list.innerHTML=""; 
+}
+
+const o_deletion=()=>{
+  //for each element in <ul> "c_list" use the .remove()
+  o_list.innerHTML=""; 
+}
+
+//event delegation : moving of finished tasks to completed list
+o_list.addEventListener("click",(e) => {
   if (e.target.tagName === "LI") {
-    c_list.appendChild(e.target);
+    c_list.appendChild(e.target); // appenchild moves the child , not a copy
   }
 });
 
-const deletion=(e)=>{
-  // now e is event obj automaticaly passed by the below call
-  if (e.target.tagName==="LI"){
-    e.target.remove();
-  }
-}
-
-
-//task deletion for completed list
-c_list.addEventListener("click",deletion)
-
-
+//task deletion for completed o_list
 button.addEventListener("click",adder)
 taskInput.addEventListener("keydown",(e)=>{if (e.key=="Enter"){
   adder();
 }
 })
+c_delete.addEventListener("click",c_deletion)
+o_delete.addEventListener("click",o_deletion)
 
 
 
